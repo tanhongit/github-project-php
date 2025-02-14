3fba54710687a8d3fa8e8eace9b19885f6de3147
Tan Nguyen <tannp27@gmail.com>
2025-02-14 14:57:09 +0700

    fix: improve color handling in single select field display

--- resources/views/md/field_types/single_select.blade.php
+++ resources/views/md/field_types/single_select.blade.php
@@ -1,5 +1,11 @@
+@php
+    $fromColor = $payload['from']['color'] ?? null;
+    $toColor = $payload['to']['color'] ?? null;
+@endphp
 @if($fromValue != null)
-**{{ $fieldName }}** has been changed from $${\color{{{ $payload['from']['color'] }}}{{ $fromValue }}$$ to $${\color{{{ $payload['to']['color'] }}}{{ $toValue }}$$.
+**{{ $fieldName }}** has been changed from $${\color{{{ $fromColor }}}{{ $fromValue }}$$ to $${\color{{{ $toColor }}}{{ $toValue }}$$.
+@elseif ($toValue != null)
+**{{ $fieldName }}** has been removed from $${\color{{{ $fromColor }}}{{ $fromValue }}}$$.
 @else
-**{{ $fieldName }}** has been set to $${\color{{{ $payload['to']['color'] }}}{{ $toValue }}$$.
+**{{ $fieldName }}** has been set to $${\color{{{ $toColor }}}{{ $toValue }}$$.
 @endif
