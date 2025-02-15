c8a452f3639e90c94acca359bbad4da635f4f926
Tan Nguyen <tannp27@gmail.com>
2025-02-15 10:00:13 +0700

    feat: add condition to check missing template for field types

--- lang/en/github-project.php
+++ lang/en/github-project.php
@@ -10,6 +10,7 @@ return [
             'denied' => 'The event was denied.',
             'action_not_found' => 'Not an action event.',
             'missing_fields' => 'Missing required fields.',
+            'missing_field_template' => 'Missing field template.',
             'status_comment_disabled' => 'Status comment is disabled.',
         ],
     ],
