90b9b23d3c324edfd4f7efee2b876fb2ea60ef4d
Tan Nguyen <tannp27@gmail.com>
2025-02-15 10:26:53 +0700

    feat: update webhook job to handle save event to cache

--- src/Jobs/ProcessWebhookEvent.php
+++ src/Jobs/ProcessWebhookEvent.php
@@ -2,26 +2,41 @@
 
 namespace CSlant\GitHubProject\Jobs;
 
+use CSlant\GitHubProject\Constants\WebHookConstant;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
 
 class ProcessWebhookEvent implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected array $eventData;
 
     /**
      * Create a new job instance.
+     *
+     * @param array $eventData
      */
-    public function __construct()
+    public function __construct(array $eventData)
     {
-        //
+        $this->eventData = $eventData;
     }
 
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle()
     {
-        //
+        $events = Cache::get(WebHookConstant::WEBHOOK_CACHE_NAME, []);
+        $events[] = $this->eventData;
+        Cache::put(WebHookConstant::WEBHOOK_CACHE_NAME, $events, now()->addSeconds(20));
+
+        if (count($events) === 1) {
+            ProcessAggregatedEvents::dispatch()->delay(now()->addSeconds(20));
+        }
     }
 }
