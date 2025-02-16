c218e9ee1d2560188321be8486d1ea1c5a0c8e6a
tanhongit <35853002+tanhongit@users.noreply.github.com>
2025-02-16 10:53:05 +0000

    Fix styling

--- src/Jobs/ProcessAggregatedEvents.php
+++ src/Jobs/ProcessAggregatedEvents.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Mail;
 
 class ProcessAggregatedEvents implements ShouldQueue
 {
